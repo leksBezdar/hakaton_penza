a85ad4ae15abd71093593f67d70f3abb871c06b2
leksBezdar <vollkovalex1991@mail.ru>
2023-10-23 13:51:09 +0400

    Try to fix render w new DB

--- src/ai_chat/routers.py
+++ src/ai_chat/routers.py
@@ -1,14 +1,17 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Request, Response
 
-from .service import AIChat
+from .service import AIManager
 
 
 router = APIRouter()
 
 
 @router.get("/get_ai_avdvice")
-def get_ai_advice(prompt: str):
-    
-    response = AIChat.get_ai_advice(prompt=prompt)
+def get_ai_advice(prompt: str, request: Request, response: Response):
+
+    AIChat = AIManager()
+    ai_chat = AIChat.ai_chat
+
+    response = ai_chat.get_ai_advice(prompt=prompt,  response=response)
     
     return response
--- src/ai_chat/service.py
+++ src/ai_chat/service.py
@@ -1,21 +1,53 @@
 import g4f
+from g4f.Provider import GeekGpt
+from time import perf_counter
+
+from fastapi import Response
 
 from sqlalchemy.ext.asyncio import AsyncSession
 
 class AIChat:
-    
-    
-    def __init__(self, db: AsyncSession):
-        self.db = db
-        
-    
+    def __init__(self):
+        self.messages = []
+
     @staticmethod
-    def get_ai_advice(prompt: str) -> str:
-        
+    def _ask_gpt(messages: list) -> str:
+        start = perf_counter()
         response = g4f.ChatCompletion.create(
-            model=g4f.models.gpt_35_turbo_16k_0613,
-            messages=[{"role": "user", "content": prompt}],
+            model=g4f.models.gpt_35_turbo,
+            messages=messages,
+            provider=GeekGpt,
         )
-        
+        end = perf_counter()
+        print(end - start)
         return response
+
+    def get_ai_advice(self, prompt: str, response: Response) -> str:
+        
+        self.messages.append({"role": "user", "content": "Разговаривай со мной только на русском"})
+        self.messages.append({"role": "user", "content": prompt})
+
+        gpt_response = self._ask_gpt(messages=self.messages)
+        self.messages.append({"role": "assistant", "content": gpt_response})
+
+        print(gpt_response)
+
+        response.set_cookie(
+        'gpt_response_context',
+        gpt_response,
+        max_age=60 * 60 * 24,
+        httponly=True
+    )
+
+        print(self.messages)
+
+        return response
+
+
+
+class AIManager:
+
+    def __init__(self) -> None:
+        self.ai_chat = AIChat()
+
     
