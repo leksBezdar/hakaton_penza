dc936a97d27d47208966e37ae461e24392ce228d
leksBezdar <vollkovalex1991@mail.ru>
2023-10-26 11:21:00 +0400

    Enhance ip_decoder

--- src/ip_decoder/service.py
+++ src/ip_decoder/service.py
@@ -1,16 +1,20 @@
 import requests
-
-
+import socket
+## getting the IP address using socket.gethostbyname() method
 class IpDecoder:
 
     def _get_ip(self):
-        response = requests.get('https://api64.ipify.org?format=json').json()
-        return response["ip"]
+        hostname = socket.gethostname()
+        ip_address = socket.gethostbyname(hostname)
+        print(ip_address)
+
+        # response = requests.get('https://api64.ipify.org?format=json').json()
+        return ip_address
 
 
     def _get_api_decoder_data(self) -> dict:
         ip_address = self._get_ip()
-        url = f'http://ip-api.com/json/{ip_address}'
+        url = f"https://api.ipgeolocation.io/ipgeo?apiKey=2c687162d4e9429fb36decef1bd5ed27&ip={ip_address}&fields=city,latitude,longitude"
         response = requests.get(url).json()
         
         return response
@@ -20,9 +24,9 @@ class IpDecoder:
         
         response = self._get_api_decoder_data()
         
-        location_data = {
-            "city": response.get("city"),
-            "latitude": response.get("lat"),
-            "longitude": response.get("lon"),
-        }
-        return location_data
+        # location_data = {
+        #     "city": response.get("city"),
+        #     "latitude": response.get("lat"),
+        #     "longitude": response.get("lon"),
+        # }
+        return response
