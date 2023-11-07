dc7490ef00f4716eefe3077847a676cb85ab4332
Alexey Volkov <vollkovalex1991@mail.ru>
2023-11-07 20:39:37 +0400

    Enhance recommendation system

--- src/recommendations/routers.py
+++ src/recommendations/routers.py
@@ -5,6 +5,8 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from ..database import get_async_session
 from .service import DatabaseManager
 
+from time import perf_counter
+
 
 router = APIRouter()
 
@@ -15,12 +17,11 @@ async def get_recommendations(
     num_films: int = 20,
     db: AsyncSession = Depends(get_async_session)):
     try:
-        
         db_manager = DatabaseManager(db)
         recommendations = db_manager.recommendations
-        
         recommendations = await recommendations.get_recommendations(user_id, num_films)
+        
         return recommendations
-    
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
+        
--- src/recommendations/service.py
+++ src/recommendations/service.py
@@ -1,9 +1,7 @@
-from typing import List
 from random import sample
 from sqlalchemy import select
 
 from sqlalchemy.ext.asyncio import AsyncSession
-from sqlalchemy.future import select
 
 from ..films.dao import FilmDAO
 from ..films.models import Film
@@ -14,12 +12,37 @@ from .config import THRESHOLD_FOR_POSITIVE_RATING
 from .config import NUM_GENRES
 
 class Recommendations:
+    """
+    Класс Recommendations предоставляет методы для генерации рекомендаций фильмов для пользователей
+    на основе их рейтингов и жанров.
+
+    Args:
+        db (AsyncSession): Сессия для работы с базой данных.
+
+    Attributes:
+        db (AsyncSession): Сессия для работы с базой данных.
+        user_positive_ratings (List[tuple]) | None: Список фильмов, оцененных пользователем положительно.
+        user_negative_ratings (List[tuple]) | None: Список фильмов, оцененных пользователем отрицательно.
+    """
     
     def __init__(self, db: AsyncSession) -> None:
         self.db = db
+        self.user_positive_ratings = None
+        self.user_negative_ratings = None
 
     async def _get_recent_ratings(self, user_id: str, limit: int = 20) -> list[tuple[int, float]]:
-    
+        
+        """
+        Получает недавние рейтинги пользователя.
+
+        Args:
+            user_id (str): Идентификатор пользователя.
+            limit (int, optional): Максимальное количество рейтингов для получения. По умолчанию 20.
+
+        Returns:
+            list[tuple[int, float]]: Список кортежей, где каждый кортеж содержит идентификатор фильма и его рейтинг.
+        """
+
         try:
             query = (
                 select(UserFilmRating.film_id, UserFilmRating.rating)
@@ -29,9 +52,7 @@ class Recommendations:
             )
                 
             result = await self.db.execute(query)            
-            rows = result.fetchall()
-
-            user_ratings = [(row[0], row[1]) for row in rows]
+            user_ratings = result.fetchall()
             
             return user_ratings
         
@@ -39,43 +60,113 @@ class Recommendations:
             print(f"Error in _get_recent_ratings: {e}")
             raise 
 
-    @staticmethod
-    async def _get_user_positive_ratings(user_ratings: list) -> list[int]:
+    async def _get_user_positive_ratings(self, user_ratings: list) -> tuple[int]:
         
-        return [film_id for film_id, rating in user_ratings if rating >= float(THRESHOLD_FOR_POSITIVE_RATING)]
-    
-    @staticmethod
-    async def _get_user_negative_ratings(user_ratings: list) -> list[int]:
-        
-        return [film_id for film_id, rating in user_ratings if rating < float(THRESHOLD_FOR_POSITIVE_RATING)]
-    
-    async def _get_suitable_films(self, user_ratings: list, all_films: list[Film]) -> list[int]:
-            
-        user_positive_ratings = await self._get_user_positive_ratings(user_ratings)
-        user_negative_ratings = await self._get_user_negative_ratings(user_ratings)
-                
-        user_rated_films = user_positive_ratings + user_negative_ratings
-        suitable_films = [film for film in all_films if film.id not in user_rated_films]
+        """
+        Возвращает список фильмов, оцененных пользователем положительно.
 
-        return suitable_films  
+        Args:
+            user_ratings (list): Список кортежей (film_id, rating).
 
-    async def _get_most_common_genres(self, user_positive_films: list) -> list[str]:
+        Returns:
+            tuple[int]: Кортеж идентификаторов фильмов, оцененных положительно.
+        """
         
-        genre_count = {}
-        for film_id in user_positive_films:
-            film = await FilmDAO.find_one_or_none(self.db, Film.id == film_id)
-            for genre in film.genres:
-                genre_count[genre] = genre_count.get(genre, 0) + 1
+        if self.user_positive_ratings:
+            return self.user_positive_ratings
+        
+        self.user_positive_ratings = tuple([film_id for film_id, rating in user_ratings if rating >= float(THRESHOLD_FOR_POSITIVE_RATING)])
+        return self.user_positive_ratings
 
-        sorted_genres = sorted(genre_count.keys(), key=lambda genre: genre_count[genre], reverse=True)
+    async def _get_user_negative_ratings(self, user_ratings: list) -> tuple[int]:
         
-        return sorted_genres[:int(NUM_GENRES)]
+        """
+        Возвращает список фильмов, оцененных пользователем отрицательно.
+
+        Args:
+            user_ratings (list): Список кортежей (film_id, rating).
+
+        Returns:
+            tuple[int]: Кортеж идентификаторов фильмов, оцененных положительно.
+        """
+        
+        if self.user_negative_ratings:
+            return self.user_negative_ratings
+        
+        self.user_negative_ratings = tuple([film_id for film_id, rating in user_ratings if rating < float(THRESHOLD_FOR_POSITIVE_RATING)])
+        return self.user_negative_ratings
     
-    async def _get_random_related_films(self, user_id: str, count: int, all_films: list[Film]) -> List[int]:
+    async def _get_suitable_films(self, user_ratings: list, all_films: tuple[Film]) -> tuple[int]:
+        
+        """
+        Возвращает фильмы, подходящие для рекомендаций пользователю.
+
+        Args:
+            user_ratings (list): Список кортежей (film_id, rating).
+            all_films (tuple[Film]): Все фильмы в базе данных.
+
+        Returns:
+            tuple[int]: Фильмы, подходящие для рекомендаций (исключая уже оцененные).
+        """
         
         try:
             
-            user_ratings = await self._get_recent_ratings(user_id=user_id)
+            user_positive_ratings = await self._get_user_positive_ratings(user_ratings)
+            user_negative_ratings = await self._get_user_negative_ratings(user_ratings)
+
+            user_rated_films = user_positive_ratings + user_negative_ratings
+            suitable_films = tuple([film for film in all_films if film.id not in user_rated_films])
+            
+            return suitable_films
+        
+        except Exception as e:
+            print(f"Error in _get_suitable_films: {e}")
+            raise 
+
+    async def _get_most_common_genres(self, user_positive_films: list, all_films: tuple[Film]) -> list[str]:
+        
+        """
+        Возвращает наиболее часто встречающиеся жанры среди фильмов, оцененных положительно пользователем.
+
+        Args:
+            user_positive_films (List[int]): Список идентификаторов фильмов, оцененных положительно.
+            all_films (tuple[Film]): Все фильмы в базе данных.
+
+        Returns:
+            List[str]: Список наиболее популярных жанров среди положительных оценок.
+        """
+        
+        try: 
+        
+            genre_count = {}
+            for film_id in user_positive_films:
+                film = next((film for film in all_films if film.id == film_id), None)
+                for genre in film.genres:
+                    genre_count[genre] = genre_count.get(genre, 0) + 1
+
+            sorted_genres = sorted(genre_count.keys(), key=lambda genre: genre_count[genre], reverse=True)
+
+            return sorted_genres[:int(NUM_GENRES)]
+        except Exception as e:
+            print(f"Error in _get_most_common_genres: {e}")
+            raise 
+    
+    async def _get_random_related_films(self, count: int, all_films: tuple[Film], user_ratings: list) -> list[int]:
+        
+        """
+        Возвращает случайные фильмы, которые связаны с предпочтениями пользователя.
+        Вызывается в случае недостатка наиболее схожих фильмов.
+
+        Args:
+            count (int): Количество недостающих фильмов для рекомендации.
+            all_films (tuple[Film]): Все фильмы в базе данных.
+            user_ratings (list): Список кортежей с оценками пользователя(film_id, rating).
+
+        Returns:
+            List[int]: Список случайных фильмов.
+        """
+        
+        try:
             suitable_films = await self._get_suitable_films(user_ratings, all_films)
 
             if len(suitable_films) <= count:
@@ -84,57 +175,119 @@ class Recommendations:
                 random_unrelated_films = sample(suitable_films, count)
 
             return random_unrelated_films
+        
         except Exception as e:
             print(f"Error in _get_random_related_films: {e}")
             raise   
 
-    async def _get_recommended_film(self, num_films: int, user_id: str, all_films: list[Film]) -> list[int]:
+    async def _get_recommended_films(self,
+        num_films: int,
+        all_films: tuple[Film],
+        user_id: str,
+        user_ratings: list) -> set[Film]:
         
-        user_ratings = await self._get_recent_ratings(user_id=user_id)
-        user_positive_films = await self._get_user_positive_ratings(user_ratings)
-        target_genres = await self._get_most_common_genres(user_positive_films)
+        """
+        Генерирует рекомендации фильмов для пользователя.
 
-        suitable_films = await self._get_suitable_films(user_ratings, all_films)
+        Args:
+            num_films (int): Количество фильмов для рекомендации.
+            all_films (tuple[Film]): Все фильмы в базе данных.
+            user_id (str): Идентификатор пользователя.
+            user_ratings (list): Список кортежей с оценками пользователя(film_id, rating).
+
+        Returns:
+            set[int]: Множество идентификаторов рекомендованных фильмов.
+        """
         
-        similar_films = set()
-        for film in suitable_films:
-                   
-            similar_films.update(await self._get_similar_films(film, target_genres, num_films))
-                                    
-        if len(similar_films) < num_films:
-            additional_count = num_films - len(similar_films)
-            similar_films = await self._get_additional_films(similar_films, user_id, additional_count)
+        try:
         
-        return similar_films  
+            user_positive_films = await self._get_user_positive_ratings(user_ratings)
+            target_genres = await self._get_most_common_genres(user_positive_films, all_films)
+
+            suitable_films = await self._get_suitable_films(user_ratings, all_films)
+
+            similar_films = set()
+            for film in suitable_films:
+
+                if len(similar_films) >= num_films:
+                    break
+                
+                similar_films.add(await self._get_similar_film(film, target_genres))
+
+            if len(similar_films) < num_films:
+                additional_count = num_films - len(similar_films)
+                similar_films = await self._get_additional_films(similar_films, user_id, additional_count, user_ratings)
+                
+            return similar_films  
+        
+        except Exception as e:
+            print(f"Error in _get_recommended_film: {e}")
+            raise 
+            
     
-    async def _get_additional_films(self, recommendations: list, user_id: str, additional_count: int) -> list:
+    async def _get_additional_films(self, recommendations: set, user_id: str, additional_count: int, user_ratings: list) -> set:
         
-        random_unrelated_films = await self._get_random_related_films(user_id, additional_count)
-        recommendations.extend(random_unrelated_films)
+        """
+        Добавляет дополнительные случайные фильмы к списку рекомендаций.
+
+        Args:
+            recommendations (set): Множество рекомендованных фильмов.
+            user_id (str): Идентификатор пользователя.
+            additional_count (int): Количество дополнительных фильмов для рекомендации.
+            user_ratings (list): Список кортежей с оценками пользователя(film_id, rating).
+
+        Returns:
+            set: Обновленное множество рекомендаций с добавленными случайными фильмами.
+        """
+        
+        random_related_films = await self._get_random_related_films(user_id, additional_count, user_ratings)
+        recommendations.add(random_related_films)
         
         return recommendations
     
-    async def _get_similar_films(self, film: Film, target_genres: list, num_films: int) -> set:
+    async def _get_similar_film(self, film: Film, target_genres: list) -> Film | None:
         
-        similar_films = set()
-        common_genres = set(film.genres) & set(target_genres)
-        similarity = len(common_genres) / len(target_genres)
+        """
+        Определяет и добавляет схожие фильмы с жанрами наподобие предпочитаемых жанров пользователя.
 
-        if similarity >= float(SIMILARITY_COEFFICIENT):
+        Args:
+            film (Film): Фильм, для которого определяется похожесть на жанры.
+            target_genres (List[str]): Список жанров, предпочитаемых пользователем.
 
-            similar_films.add(film)
+        Returns:
+            Film or None: Фильм, если он похож на предпочитаемые жанры, или None, если не похож.
+        """
+        
+        try:
+            common_genres = set(film.genres) & set(target_genres)
+            similarity = len(common_genres) / len(target_genres)
 
-            if len(similar_films) >= num_films:
-                return similar_films
-            
-        return similar_films
+            if similarity >= float(SIMILARITY_COEFFICIENT):
+                return film
+        
+        except Exception as e:
+            print(f"Error in _get_similar_film: {e}")
+            raise
 
-    async def get_recommendations(self, user_id: str, num_films: int) -> List[Film]:
+    async def get_recommendations(self, user_id: str, num_films: int) -> list[Film]:
+        
+        """
+        Генерирует рекомендации фильмов для пользователя.
+
+        Args:
+            user_id (str): Идентификатор пользователя.
+            num_films (int): Количество фильмов для рекомендации.
+
+        Returns:
+            List[Film]: Список рекомендованных фильмов.
+        """
+        
         try: 
-            print(1)
-            all_films = await FilmDAO.find_all(self.db)
-            recommendations = await self._get_recommended_film(num_films, user_id, all_films)
-            print(2)
+            
+            all_films = tuple(await FilmDAO.find_all(self.db))
+            user_ratings = await self._get_recent_ratings(user_id=user_id)     
+            recommendations = await self._get_recommended_film(num_films, all_films, user_id, user_ratings)
+            
             return recommendations
         
         except Exception as e:
